eb7390624e4ca75ef8321b77b76020ff42ffe513
TomChen <tom.chen@lightningww.com>
2024-06-27 13:02:35 +0800

    測試

--- Shop/Shop/obj/Debug/net6.0/Shop.AssemblyInfo.cs
+++ Shop/Shop/obj/Debug/net6.0/Shop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7bc355a3e3972e71145597eff8ca1e3e78090d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
